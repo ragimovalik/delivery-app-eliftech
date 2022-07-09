eaf9b8a8bbadf48990cd0f248fedb140ea941080
alik.ragimov <ragimovalik@gmail.com>
2022-07-09 16:03:22 +0300

    Pages added

---
+++ src/views/ShoppingCartPage/ShoppingCartPage.jsx
@@ -0,0 +1,9 @@
+const ShoppingCartPage = () => {
+  return (
+    <div>
+      <h2>Shopping Cart Page</h2>
+    </div>
+  );
+};
+
+export default ShoppingCartPage;
---
+++ src/views/ShopsPage/ShopsPage.jsx
@@ -0,0 +1,12 @@
+import Shops from '../../modules/GoodsBox/ShopsList/Shops';
+
+const ShopsPage = () => {
+  return (
+    <div>
+      <h2>Shops Page</h2>
+      <Shops />
+    </div>
+  );
+};
+
+export default ShopsPage;
