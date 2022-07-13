e58c0ddf17c6bf305b94e9cf63d844e7ce3deb5e
alik.ragimov <ragimovalik@gmail.com>
2022-07-13 11:04:58 +0300

    Some styles and vars added

--- src/modules/GoodsBox/GoodsList/GoodsList.jsx
+++ src/modules/GoodsBox/GoodsList/GoodsList.jsx
@@ -4,7 +4,7 @@ import styles from './GoodsList.module.css';
 const GoodsList = ({ goods = [], title, addToCartHandler }) => {
   return (
     <div className={styles.wrapper}>
-      <p>{title}</p>
+      <h3 className={styles.goods__title}>{title}</h3>
       <ul className={styles.goods}>
         {goods.map(item => (
           <GoodsItem key={item._id} item={item} onClick={addToCartHandler} />
